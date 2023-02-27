e792ef335941ddc634680be204402195a56671fb
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-02-27 21:04:05 +0530

    Add Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full text-center bg-gradient-to-b from-gray-800 to-gray-600 pb-1 text-white">
+      Made with ❤️ by Kirthi Bagrecha Jain
+    </div>
+  );
+};
+
+export default Footer;
