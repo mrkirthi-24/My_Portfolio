6e1f0c703baa329de3c278bec5aa903d075875dc
mrkirthi-24 <kirthibagrecha4@gmail.com>
2023-04-17 16:06:30 +0530

    fix bugs

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -108,7 +108,7 @@ const Experience = () => {
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
             >
-              <img src={src} alt="" className="w-20 mx-auto" />
+              <img src={src} alt="" className="w-20 mx-auto rounded-full" />
               <p className="mt-4">{title}</p>
             </div>
           ))}
